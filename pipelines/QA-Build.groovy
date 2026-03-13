5df4a99b6d05b60d3814f40a797cf51c3119f07a
krimeshshah <krimeshshah@gmail.com>
2026-03-13 14:06:21 +0530

    Updated kanikobased pipeline

--- pipelines/QA-Build.groovy
+++ pipelines/QA-Build.groovy
@@ -1,41 +1,63 @@
-@Library('my-shared-library@feature/ks/shared-library') _
+@Library('my-shared-library@feature/ks/flaskapp-build') _
+
+// pipeline {
+//     agent {label "k8s-agent"}
+//     stages {
+//         stage("hellowrld-test") {
+//             steps {
+//                 helloWorld()
+//             }
+//         }
+//     }
+// }
 
 pipeline {
-    agent {label "k8s-agent"}
+    agent {
+        kubernetes {
+            label 'kaniko'
+        }
+    }
+
     stages {
-        stage("hellowrld-test") {
+        stage('Build') {
             steps {
-                helloWorld()
+                kanikoBuild(
+                    image: 'myrepo/flask',
+                    tag: env.BUILD_NUMBER
+                )
             }
         }
     }
 }
 
+
 // pipeline {
-//     agent any
+//     agent { label 'k8s-agent' }
+
+//     parameters {
+//         string(name: 'IMAGE_TAG', defaultValue: '', description: 'Optional: Provide semantic version (e.g., 1.0.0). Leave empty for auto short SHA tag.')
+//         string(name: 'BRANCH', defaultValue: '', description: 'Optional: Provide Source Code Branch to be built')
+//     }
 
 //     environment {
-//         REPO_URL         = 'https://github.com/krimeshshah/python-flaskapp.git'
-//         BRANCH           = 'develop'
-//         AWS_REGION       = 'us-east-1'
-//         ECR_REPO         = '806153319059.dkr.ecr.us-east-1.amazonaws.com/flaskapp'
-//         AWS_CRED_ID      = 'aws-ecr-jenkins-creds'
-//         IMAGE_TAG        = "build-${env.BUILD_NUMBER}"
-//     }
+//         REPO_URL    = 'https://github.com/krimeshshah/python-flaskapp.git'
+//         AWS_REGION  = 'us-east-1'
+//         ECR_REPO    = '806153319059.dkr.ecr.us-east-1.amazonaws.com/pyproject/flaskapp'
+//         AWS_CRED_ID = 'aws-ecr-jenkins-creds'
+//      }
 
 //     stages {
 //         stage('Build and Push to ECR') {
 //             steps {
-//                 script {
-//                     qabuild.buildAndPushImage([
-//                         repoUrl: REPO_URL,
-//                         branch: BRANCH,
-//                         awsRegion: AWS_REGION,
-//                         ecrRepo: ECR_REPO,
-//                         awsCredentialsId: AWS_CRED_ID,
-//                         imageTag: IMAGE_TAG
-//                     ])
-//                 }
+//                 // Directly call shared library step
+//                 qaBuild(
+//                     repoUrl: REPO_URL,
+//                     branch: params.BRANCH,
+//                     awsRegion: AWS_REGION,
+//                     ecrRepo: ECR_REPO,
+//                     awsCredentialsId: AWS_CRED_ID,
+//                     imageTag: params.IMAGE_TAG
+//                 )
 //             }
 //         }
 //     }
---
+++ vars/kanikoBuild.groovy
@@ -0,0 +1,17 @@
+def call(Map config = [:]) {
+
+    def image      = config.image
+    def tag        = config.tag ?: env.BUILD_NUMBER
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context    = config.context ?: env.WORKSPACE
+
+    container('kaniko') {
+        sh """
+          /kaniko/executor \
+            --context ${context} \
+            --dockerfile ${dockerfile} \
+            --destination ${image}:${tag} \
+            --cache=true
+        """
+    }
+}
